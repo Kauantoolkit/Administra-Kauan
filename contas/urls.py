aba20e6beae48b4e6105fc4278e62626dc06fcda
Rafael Sampaio <160675357+rafaeljuniora@users.noreply.github.com>
2025-11-08 22:21:36 -0300

    feat: nova dashboard e gitignore

--- contas/urls.py
+++ contas/urls.py
@@ -1,12 +1,11 @@
-# em contas/urls.py (O ARQUIVO NOVO)
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    # Quando alguém visitar /cadastro/, chame a função cadastro_view
     path('cadastro/', views.cadastro_view, name='cadastro'),
     path('login/', views.login_view, name='login'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('clientes/', views.clientes_view, name='clientes'),
 ]
