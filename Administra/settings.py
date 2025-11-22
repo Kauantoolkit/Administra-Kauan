b302be27493c7e18d633ed697da4ca8c67c205d3
Kauan Cristian <107585090+Kauantoolkit@users.noreply.github.com>
2025-11-22 02:23:41 -0300

    a

--- Administra/settings.py
+++ Administra/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'contas.apps.ContasConfig',
-    'funcionarios.apps.FuncionariosConfig',
-]
+    'funcionarios.apps.FuncionariosConfig',]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'Administra.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
