7f132f1e30fd88f2effac84cc3243449bbeb0c44
Rafael Sampaio <160675357+rafaeljuniora@users.noreply.github.com>
2025-11-23 01:56:42 -0300

    c13 - Tela de vendas

---
+++ vendas/views.py
@@ -0,0 +1,110 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from django.utils import timezone
+from .models import Venda, ItemVenda
+from .forms import VendaForm, ItemVendaFormSet
+from contas.models import MovimentoEstoque 
+
+@login_required
+def lista_vendas(request):
+    vendas = Venda.objects.all().order_by('-data_venda')
+    return render(request, 'vendas/lista_vendas.html', {'vendas': vendas})
+
+@login_required
+def criar_venda(request):
+    if request.method == 'POST':
+        form = VendaForm(request.POST)
+        formset = ItemVendaFormSet(request.POST)
+        
+        if form.is_valid() and formset.is_valid():
+            with transaction.atomic():
+                venda = form.save(commit=False)
+                venda.data_venda = timezone.now()
+                venda.status = 'fechada'
+                venda.save()
+                
+                items = formset.save(commit=False)
+                total_venda = 0
+                
+                for item in items:
+                    item.venda = venda
+                    item.preco_unitario = item.produto.venda 
+                    item.save()
+                    total_venda += item.subtotal()
+                    
+                    MovimentoEstoque.objects.create(
+                        produto=item.produto,
+                        tipo_movimento='SAIDA',
+                        quantidade=item.quantidade,
+                        observacao=f"Venda #{venda.id}",
+                        usuario=request.user
+                    )
+                
+                venda.total = total_venda
+                venda.save()
+                
+                cliente = venda.cliente
+                cliente.ultima_compra = venda.data_venda.date()
+                cliente.valor_total_comprado += total_venda 
+                cliente.save()
+                
+                return redirect('lista_vendas')
+    else:
+        form = VendaForm()
+        formset = ItemVendaFormSet()
+    
+    return render(request, 'vendas/criar_venda.html', {
+        'form': form, 'formset': formset, 'titulo': 'Nova Venda'
+    })
+
+@login_required
+def editar_venda(request, pk):
+    venda = get_object_or_404(Venda, pk=pk)
+    
+    if request.method == 'POST':
+        form = VendaForm(request.POST, instance=venda)
+        formset = ItemVendaFormSet(request.POST, instance=venda)
+        
+        if form.is_valid() and formset.is_valid():
+            with transaction.atomic():
+                form.save()
+                
+                items = formset.save(commit=False)
+                
+                for item in items:
+                    item.venda = venda
+                    if not item.preco_unitario:
+                         item.preco_unitario = item.produto.venda
+                    item.save()
+                
+                venda.total = sum([i.subtotal() for i in venda.itens.all()])
+                venda.save()
+                
+                return redirect('lista_vendas')
+    else:
+        form = VendaForm(instance=venda)
+        formset = ItemVendaFormSet(instance=venda)
+    
+    return render(request, 'vendas/criar_venda.html', {
+        'form': form, 'formset': formset, 'titulo': f'Editar Venda #{venda.id}'
+    })
+
+@login_required
+def excluir_venda(request, pk):
+    venda = get_object_or_404(Venda, pk=pk)
+    
+    if request.method == 'POST':
+        with transaction.atomic():
+            for item in venda.itens.all():
+                MovimentoEstoque.objects.create(
+                    produto=item.produto,
+                    tipo_movimento='ENTRADA',
+                    quantidade=item.quantidade,
+                    observacao=f"Estorno de Venda Excluída #{venda.id}",
+                    usuario=request.user
+                )
+            venda.delete()
+        return redirect('lista_vendas')
+    
+    return render(request, 'vendas/confirmar_exclusao.html', {'object': venda})
