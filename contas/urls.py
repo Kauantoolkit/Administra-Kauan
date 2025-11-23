ef20340544e056d79e1197db3ae119f4c4d411ae
Emerson <emerson_corppegoraro@outlook.com>
2025-11-22 22:38:13 -0300

    feat: corrigindo erros imundos c-10

--- contas/urls.py
+++ contas/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('cadastro/', views.cadastro_view, name='cadastro'),
@@ -9,3 +11,5 @@ urlpatterns = [
     path('clientes/', views.clientes_view, name='clientes'),
     path('relatorios/', views.relatorios, name='relatorios'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
