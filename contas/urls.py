0dc1d564aff422cba8cf2cc304b9c6aa32821ff8
Emerson <emerson_corppegoraro@outlook.com>
2025-11-23 13:47:18 -0300

    feat: arrumando as paradas versão 3000

--- contas/urls.py
+++ contas/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('clientes/', views.clientes_view, name='clientes'),
     path('relatorios/', views.relatorios, name='relatorios'),
+    path('estoque/', views.estoque_view, name='estoque'),
+    path('estoque/entrada/', views.entrada_estoque_geral_view, name='entrada_estoque'),
+    path('estoque/novo/', views.novo_produto_view, name='novo_produto'),
+    
 ]
