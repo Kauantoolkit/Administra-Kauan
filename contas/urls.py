6d722450f23a6a7064609dc7b23cfbda4c5f4d2b
Rafael Sampaio <160675357+rafaeljuniora@users.noreply.github.com>
2025-11-22 21:10:06 -0300

    Revert "Merge branch 'main' into c13---Tela-de-vendas"
    This reverts commit 16a2854cf3e12557af8baea7fd88a86909501356, reversing
    changes made to f48f9c52672680b2baf1ae0085440539383c05b6.

--- contas/urls.py
+++ contas/urls.py
@@ -11,13 +11,4 @@ urlpatterns = [
     path('fornecedores/criar/', views.fornecedor_criar, name='fornecedor_criar'),
     path('fornecedores/<int:pk>/editar/', views.fornecedor_editar, name='fornecedor_editar'),
     path('fornecedores/<int:pk>/deletar/', views.fornecedor_deletar, name='fornecedor_deletar'),
-    path('estoque/', views.estoque_view, name='estoque'),
-    path('estoque/entrada/', views.entrada_estoque_geral_view, name='entrada_estoque'),
-    path('produtos/novo/', views.novo_produto_view, name='novo_produto'),
-    path('produtos/<int:pk>/detalhe/', views.detalhe_produto_view, name='detalhe_produto'),
-    path('produtos/<int:pk>/editar/', views.editar_produto_view, name='editar_produto'),
-    path('produtos/<int:pk>/adicionar-estoque/', views.adicionar_estoque_view, name='entrada_produto'),
-    path('produtos/<int:pk>/excluir/', views.excluir_produto_view, name='excluir_produto'),
-    path("estoque/detalhes/<int:pk>/", views.detalhes_produto_ajax, name="detalhes_produto_ajax"),
-
 ]
